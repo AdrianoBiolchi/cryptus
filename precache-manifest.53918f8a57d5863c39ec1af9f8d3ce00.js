69f817dc01e51636c4554f9b2f9ba0fae4402bcb
unknown <adrianobiolchi@outlook.com>
2019-04-30 15:02:22 -0300

    Updates

---
+++ precache-manifest.53918f8a57d5863c39ec1af9f8d3ce00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0061864c1793505bccd8bd1af97bb5a9",
+    "url": "/cryptus/index.html"
+  },
+  {
+    "revision": "df14ae0f7f6b45277921",
+    "url": "/cryptus/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "968dfef26b7ed5c746b2",
+    "url": "/cryptus/static/css/main.e812c5ae.chunk.css"
+  },
+  {
+    "revision": "df14ae0f7f6b45277921",
+    "url": "/cryptus/static/js/2.ff28d976.chunk.js"
+  },
+  {
+    "revision": "968dfef26b7ed5c746b2",
+    "url": "/cryptus/static/js/main.45c2d83b.chunk.js"
+  },
+  {
+    "revision": "5bc8bd64eeb18b47397f",
+    "url": "/cryptus/static/js/runtime~main.55298e34.js"
+  }
+]);
