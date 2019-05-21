60965832c58bc79fa48c0444a2085163c0075b8a
Adriano Biolchi <adrianobiolchi@outlook.com>
2019-05-21 09:57:02 -0300

    images

--- src/components/Body.js
+++ src/components/Body.js
@@ -130,7 +130,7 @@ class Body extends React.Component{
         <div className="row mt-5 mt-xs-4">
             <div className="col-12 mb-3">
                  <div className="card-deck custom-card-deck">
-                    <PriceCard header="Bitcoin(BTC)" src={'/bitcoin.png'} alt="fireSpot" label="(Price in USD)"   value={this.state.btcusd} />
+                    <PriceCard header="Bitcoin(BTC)" src={'../../public/bitcoin.png'} alt="fireSpot" label="(Price in USD)"   value={this.state.btcusd} />
                     <PriceCard header="Litecoin(LTC)"   src={'/litecoin.png'} alt="fireSpot" label="(Price in USD)"  value={this.state.ltcusd}/>
                     <PriceCard header="Ethereum(ETH)" src={'/ethereum.png'} alt="fireSpot" label="(Price in USD)"    value={this.state.ethusd}/>
                     
